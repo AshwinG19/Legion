033230f50a28f6d7cdb3110ad6cc2ab0d9a59ee2
AshwinG19 <46891487+AshwinG19@users.noreply.github.com>
2019-01-25 16:03:14 +0530

    Create prime_range.py

---
+++ prime_range.py
@@ -0,0 +1,9 @@
+n,q=map(int,input().split())
+print("Prime numbers between",n,"and",q,"are:")
+for num in range(n,q + 1):
+   if num > 1:
+       for i in range(2,num):
+           if (num % i) == 0:
+               break
+       else:
+           print(num)
